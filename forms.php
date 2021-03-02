ea446c2e9afc9ff34d5886975decf70a19b14fbf
Nick Laptop <NIcholas.A.Newell@gmail.com>
2021-03-01 18:48:30 -0600

    Phase 1 Kickoff
    Kickoff using a template from a different project

---
+++ forms.php
@@ -0,0 +1,27 @@
+<?php
+  $pageTitle = 'Forms';
+  $keywords = 'Forms';
+  $description = 'Forms';
+  include $_SERVER['DOCUMENT_ROOT']."/templates/header.php";
+?>
+
+
+ <header class="masthead bg-primary text-center" style="height: 800px">
+      <div class="container" >
+        <h1 class="text-uppercase mb-0">Forms</h1>
+        <hr class="star-light">
+      </div>
+      <div class="container">
+        <h4 class="text-uppercase">
+          <a style="color: #212529" href="/forms/socialMediaForm.php"> Social Media Form </a>
+        </h4>
+
+        
+      </div>
+  </header>
+
+  
+
+<?php
+  include $_SERVER['DOCUMENT_ROOT']."/templates/footer.php";
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  $pageTitle = 'Prayer Triage';
+  $keywords = 'Prayer Triage';
+  $description = 'Prayer Triage';
+  include $_SERVER['DOCUMENT_ROOT']."/templates/header.php";
+?>
+
+    <header class="masthead bg-primary text-center">
+      <div class="container">
+            <h1 class="text-uppercase mb-0">Main Page!</h1>
+      </div>
+    </header>
+
+<?php
+  include $_SERVER['DOCUMENT_ROOT']."/templates/footer.php";
+?>
